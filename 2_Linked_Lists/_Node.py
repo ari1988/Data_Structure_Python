1c63d26b4cb3216bddaae84b6ae9ada398e45d96
ArvindSingh1 <arvind_singh1@epam.com>
2024-01-21 04:42:56 +0530

    👌 IMPROVE: Updated Node class for Doubly Linked List

--- 2_Linked_Lists/_Node.py
+++ 2_Linked_Lists/_Node.py
@@ -2,5 +2,5 @@ class Node:
   def __init__(self, data):
     self.data = data # Data field
     self.next_element = None # Pointer to next node
-
+    self.previous_element = None
 ## the Node class has two variables. data contains your specified value and nextElement is the pointer to the next element of the list.
