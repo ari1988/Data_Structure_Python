eecb40c5d0b1cc1980ba9c5ee30afe49a97ced31
ArvindSingh1 <arvind_singh1@epam.com>
2024-01-21 05:09:46 +0530

    👌 IMPROVE: Updated Linked List __init__ method for Doubly Linked List

--- 2_Linked_Lists/_LinkedList.py
+++ 2_Linked_Lists/_LinkedList.py
@@ -4,6 +4,7 @@ from _Node import Node
 class LinkedList:
     def __init__(self):
         self.head_node = None
+        self.tail_node = None
 
     def get_head(self):
         return self.head_node
