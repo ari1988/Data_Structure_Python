408ec6b986a0963bf1efc0a904b37ab597b82c07
ArvindSingh1 <arvind_singh1@epam.com>
2024-01-28 20:39:11 +0530

    📦 NEW: Challenge - 3: Reversing First k Elements of Queue

---
+++ 3_Stacks_n_Queues/Challenge_3.py
@@ -0,0 +1,35 @@
+from Queue import MyQueue
+from Stack import MyStack
+
+
+def reverseK(queue, k):
+    if queue.is_empty() or k > queue.size() or k < 0:
+        return None
+    stack = MyStack()
+    for i in range(k):
+        stack.push(queue.dequeue())
+    while stack.is_empty() is False:
+        queue.enqueue(stack.pop())
+    size = queue.size()
+    for i in range(size - k):
+        queue.enqueue(queue.dequeue())
+    return None
+
+if __name__ == "__main__" :
+    # testing our logic
+    queue = MyQueue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    queue.enqueue(4)
+    queue.enqueue(5)
+    queue.enqueue(6)
+    queue.enqueue(7)
+    queue.enqueue(8)
+    queue.enqueue(9)
+    queue.enqueue(10)
+    print("the queue before reversing:")
+    print(queue.items)
+    reverseK(queue, 5)
+    print("the queue after reversing:")
+    print(queue.items)
